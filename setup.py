b42c55ad5e1d44c988edce8cf536f2ff0a69509d
caleb15 <caleb@15five.com>
2020-03-24 14:27:25 -0700

    blackify setup

--- setup.py
+++ setup.py
@@ -7,24 +7,26 @@
 import os
 import sys
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 # circleci.py version
 VERSION = "0.1.0"
 
+
 def readme():
     """print long description"""
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 class VerifyVersionCommand(install):
     """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
+
+    description = "verify that the git tag matches our version"
 
     def run(self):
-        tag = os.getenv('CIRCLE_TAG')
+        tag = os.getenv("CIRCLE_TAG")
 
         if tag != VERSION:
             info = "Git tag: {0} does not match the version of this app: {1}".format(
@@ -32,36 +34,33 @@ class VerifyVersionCommand(install):
             )
             sys.exit(info)
 
+
 setup(
-    name='healthchecks_manager',
+    name="healthchecks_manager",
     version=VERSION,
     description="Library for automatic registration and pinging of https://healthchecks.io/ healthchecks.",
     long_description=readme(),
     url="https://github.com/15five/healthchecks-manager",
     author="Caleb Sparks",
-    author_email='caleb@15five.com',
+    author_email="caleb@15five.com",
     license="MIT",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
-        'License :: OSI Approved :: MIT License',
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Natural Language :: English',
+        "Natural Language :: English",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Internet",
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    keywords='healthchecks healthchecks.io registration creation',
+    keywords="healthchecks healthchecks.io registration creation",
     packages=find_packages(),
-    install_requires=[
-        'requests==2.20.1',
-    ],
-    python_requires='>=3',
-    cmdclass={
-        'verify': VerifyVersionCommand,
-    }
+    install_requires=["requests==2.20.1",],
+    python_requires=">=3",
+    cmdclass={"verify": VerifyVersionCommand,},
 )
