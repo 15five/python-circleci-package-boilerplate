a9916eebfbf88ea15dc25617f646846461be0d31
caleb15 <caleb@15five.com>
2020-03-25 11:01:16 -0700

    deploy new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 
 class VerifyVersionCommand(install):
