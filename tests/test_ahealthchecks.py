474a48f1e2a83f2f3aa3a8eb319590b9f1a21c49
caleb15 <caleb@15five.com>
2020-04-02 16:15:55 -0700

    rename to be more succint

--- tests/test_ahealthchecks.py
+++ tests/test_ahealthchecks.py
@@ -1,4 +1,4 @@
-import healthchecks_manager.ahealthchecks
+from healthchecks_manager import manager
 from healthchecks_manager.healthchecks_api_wrapper import API_URL_BASE
 import pytest
 import responses
@@ -22,22 +22,22 @@ class MockRedisCache(dict):
 
 
 def test_get_endpoint():
-    healthchecks_manager.ahealthchecks.cache = {"foo": "mock url"}
-    endpoint = healthchecks_manager.ahealthchecks.get_endpoint("foo", {})
+    manager.cache = {"foo": "mock url"}
+    endpoint = manager.get_endpoint("foo", {})
     assert endpoint == "mock url"
-    healthchecks_manager.ahealthchecks.cache = {}
+    manager.cache = {}
 
 
 def test_get_endpoint_with_cache_override():
-    healthchecks_manager.ahealthchecks.cache = MockRedisCache()
-    endpoint = healthchecks_manager.ahealthchecks.get_endpoint("foo1", {})
+    manager.cache = MockRedisCache()
+    endpoint = manager.get_endpoint("foo1", {})
     assert endpoint == "mock url1"
-    healthchecks_manager.ahealthchecks.cache = {}
+    manager.cache = {}
 
 
 def test_error_if_no_api_key():
     with pytest.raises(HTTPError):
-        healthchecks_manager.ahealthchecks.get_endpoint("barnacle", {})
+        manager.get_endpoint("barnacle", {})
 
 
 @responses.activate
@@ -51,7 +51,7 @@ def test_get_endpoint_if_not_cached_but_exists_in_website():
             ]
         },
     )
-    endpoint = healthchecks_manager.ahealthchecks.get_endpoint("spongebob", {})
+    endpoint = manager.get_endpoint("spongebob", {})
     assert endpoint == "https://hc-ping.com/55555555"
 
 
@@ -66,13 +66,13 @@ def test_get_endpoint_if_does_not_exist():
         API_URL_BASE + "/checks/",
         json={"ping_url": "https://hc-ping.com/44444444"},
     )
-    endpoint = healthchecks_manager.ahealthchecks.get_endpoint("squidward", {})
+    endpoint = manager.get_endpoint("squidward", {})
     assert endpoint == "44444444"
 
 
 @responses.activate
 def test_get_endpoint_if_does_not_exist_with_cache_override():
-    healthchecks_manager.ahealthchecks.cache = MockRedisCache()
+    manager.cache = MockRedisCache()
     responses.add(
         responses.GET, API_URL_BASE + "/checks/", json={"checks": []},
     )
@@ -81,9 +81,9 @@ def test_get_endpoint_if_does_not_exist_with_cache_override():
         API_URL_BASE + "/checks/",
         json={"ping_url": "https://hc-ping.com/44444444"},
     )
-    endpoint = healthchecks_manager.ahealthchecks.get_endpoint("foo2", {})
+    endpoint = manager.get_endpoint("foo2", {})
     assert endpoint == "44444444"
-    assert healthchecks_manager.ahealthchecks.cache["foo2"] == "44444444"
+    assert manager.cache["foo2"] == "44444444"
 
 
 @responses.activate
@@ -91,7 +91,7 @@ def test_create_check():
     responses.add(
         responses.POST, API_URL_BASE + "/checks/", json={"ping_url": "foo"},
     )
-    endpoint = healthchecks_manager.ahealthchecks.create_check("")
+    endpoint = manager.create_check("")
     assert endpoint == "foo"
 
 
@@ -105,19 +105,17 @@ def test_create_check_with_channel():
     responses.add(
         responses.POST, API_URL_BASE + "/checks/", json={"ping_url": "foo"},
     )
-    endpoint = healthchecks_manager.ahealthchecks.create_check(
-        "fah", {"channels": ["slack"]}
-    )
+    endpoint = manager.create_check("fah", {"channels": ["slack"]})
     assert "foo" == endpoint
     assert "112536" in responses.calls[1].request.body
 
 
 @responses.activate
 def test_default_creation_params_can_be_overridden():
-    healthchecks_manager.ahealthchecks.default_creation_params["timeout"] = 5326
+    manager.default_creation_params["timeout"] = 5326
     responses.add(
         responses.POST, API_URL_BASE + "/checks/", json={"ping_url": "foo"},
     )
-    healthchecks_manager.ahealthchecks.create_check("fah", {"timeout": 142})
+    manager.create_check("fah", {"timeout": 142})
     assert "5326" not in responses.calls[0].request.body
     assert "142" in responses.calls[0].request.body
